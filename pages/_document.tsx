08c90c55e110cfb4e4b246d58e0e0e320be9b4e1
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-02-01 20:10:33 +0800

    feat: update twitter card

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,27 +8,27 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Simplify your chat content in seconds."
           />
-          <meta property="og:site_name" content="twitterbio.com" />
+          <meta property="og:site_name" content="Chat Simplifier" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Simplify your chat content in seconds."
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="Chat Simplifier" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="Chat Simplifier" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Simplify your chat content in seconds."
           />
           <meta
             property="og:image"
-            content="https://twitterbio.com/og-image.png"
+            content="https://chat-simplifier.vercel.app/og-image.png"
           />
           <meta
             name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
+            content="https://chat-simplifier.vercel.app/og-image.png"
           />
         </Head>
         <body>
