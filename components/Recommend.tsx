3205e6912d1283bff6b011faa799c68f24a17e58
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-02-12 12:02:29 +0800

    chore: update recommend

--- components/Recommend.tsx
+++ components/Recommend.tsx
@@ -7,38 +7,48 @@ export default function Recommend() {
       <h2 className=" text-slate-400 mb-4">{t('recommend')}</h2>
       <ul className="grid grid-cols-2 md:grid-cols-4 gap-4 text-sm">
         <li>
-          <a href="https://readpilot.vercel.app/" className="recommend-container">
-            Read Pilot
+          <a href="https://www.twitterbio.com/" target="_blank" className="recommend-container">
+            TwitterBio
           </a>
         </li>
         <li>
-          <a href="https://bing-chat.vercel.app/" className="recommend-container">
-            Bing Chat
-          </a>
-        </li>
-        <li>
-          <a href="https://chattytutor.vercel.app/" className="recommend-container">
-            Chatty Tutor
-          </a>
-        </li>
-        <li>
-          <a href="https://graphgpt.vercel.app/" className="recommend-container">
-            GraphGPT
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitterbio.com/" className="recommend-container">
-            Twitter Bio
-          </a>
-        </li>
-        <li>
-          <a href="https://email-helper.vercel.app/" className="recommend-container">
+          <a href="https://email-helper.vercel.app/" target="_blank" className="recommend-container">
             Email Helper
           </a>
         </li>
         <li>
-          <a href="https://www.teach-anything.com/" className="recommend-container">
+          <a href="https://www.teach-anything.com/" target="_blank" className="recommend-container">
             Teach Anything
+          </a>
+        </li>
+        <li>
+          <a href="https://siteexplainer.vercel.app/" target="_blank" className="recommend-container">
+          SiteExplainer
+          </a>
+        </li>
+        <li>
+          <a href="https://www.dearaibby.com/" target="_blank" className="recommend-container">
+            Dear Aibby
+          </a>
+        </li>
+        <li>
+          <a href="https://www.meout.app/" target="_blank" className="recommend-container">
+            Meout
+          </a>
+        </li>
+        <li>
+          <a href="https://text-summarizer-seven.vercel.app/" target="_blank" className="recommend-container">
+            Text Summarizer
+          </a>
+        </li>
+        <li>
+          <a href="https://lyrics-explainer.vercel.app/" target="_blank" className="recommend-container">
+            Lyrics Explainer
+          </a>
+        </li>
+        <li>
+          <a href="https://email-simplify.vercel.app/" target="_blank" className="recommend-container">
+            Email Simplify
           </a>
         </li>
       </ul>
